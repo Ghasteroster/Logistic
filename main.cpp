0402a8272eb08dba3841a113937a470d8da2b830
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-12-09 16:53:11 +0300

    feat: add some lines for working with MPI, general procedures.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ double logisticStep(double, double);
 double solveLogistic(double, double, int) {
 
 int main() {
+	MPI_Init(&argc, &argv);
+
+	runMPI(argc, argv);
+
+	MPI_Finalize();
 
 	return 0;
 }
@@ -19,4 +24,10 @@ double solveLogistic(double r, double x0, int N) {
 		x = logisticStep(r, x);
 	
 	return x;
+}
+
+void runMPI(int argc, char** argv) {
+	int rank, size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 }
