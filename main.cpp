e8fefd3b537f2045609663be7a05bb17a4b76cf6
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-12-09 16:51:49 +0300

    feat: add a function for the calculation of N steps.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 double logisticStep(double, double);
+double solveLogistic(double, double, int) {
 
 int main() {
 
@@ -9,4 +10,13 @@ int main() {
 
 double logisticStep(double r, double x) {
 	return r * x * (1.0 - x);
+}
+
+double solveLogistic(double r, double x0, int N) {
+	double x = x0;
+
+	for (int n = 0; n < N; n++)
+		x = logisticStep(r, x);
+	
+	return x;
 }
